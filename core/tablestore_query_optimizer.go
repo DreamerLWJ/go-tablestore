f313023f131c58a139ae672d502539c4745b7698
luowenjun@inke.cn <luowenjun@inke.cn>
2023-05-07 15:41:09 +0800

    add optimizer

--- core/tablestore_query_optimizer.go
+++ core/tablestore_query_optimizer.go
@@ -1,6 +1,7 @@
 package core
 
 // QueryOptimizer 查询优化器
+// TODO 带有索引下推的优化器？
 type QueryOptimizer interface {
 	GeneratePlan(FilterCond map[string]string, availableIdx []Index)
 }
@@ -8,10 +9,83 @@ type QueryOptimizer interface {
 type SimpleEqQueryOptimizer struct {
 }
 
-func (s SimpleEqQueryOptimizer) GeneratePlan(FilterCond map[string]string, availableIdx []Index) {
-	// gather
+func NewSimpleEqQueryOptimizer() *SimpleEqQueryOptimizer {
+	return &SimpleEqQueryOptimizer{}
 }
+
+// GeneratePlan 初期的优化器，仅支持等值查询判断；对于范围查询可以先定位到
+func (s SimpleEqQueryOptimizer) GeneratePlan(colNames []string, tbInfo Table) (res ExecutionPlan) {
+	var maxHitIdx Index        // 命中最长的索引
+	maxHitIdxPrefixLength := 0 // 命中最长的索引的前缀长度
+	var maxHitCombine []string // 命中最长的列组合
+
+	combines := permute(colNames)
+	for _, index := range tbInfo.Indexs {
+		for _, combine := range combines {
+			hitIdxLength := 0
+			for i := 0; i < len(index.ColumnNames) && i < len(combine); i++ {
+				if index.ColumnNames[i] == combine[i] {
+					hitIdxLength++
+				}
+			}
+
+			if hitIdxLength > maxHitIdxPrefixLength {
+				maxHitIdxPrefixLength = hitIdxLength
+				maxHitIdx = index
+				maxHitCombine = combine
+			}
+		}
+	}
+	// gather
+	if maxHitIdxPrefixLength != 0 {
+		res.PlanType = Idx
+		res.Idx = maxHitIdx
+		res.ColumnsCombine = maxHitCombine
+		res.IdxPrefixLength = maxHitIdxPrefixLength
+	} else {
+		res.PlanType = All
+	}
+	return
+}
+
+type PlanType int
+
+const (
+	All     PlanType = iota + 1 // 全搜索
+	Idx                         // 索引搜索
+	Primary                     // 主键搜索
+)
 
 // ExecutionPlan 执行计划
 type ExecutionPlan struct {
+	PlanType        PlanType
+	Idx             Index
+	IdxPrefixLength int
+	ColumnsCombine  []string
+}
+
+func permute(strs []string) [][]string {
+	var res [][]string
+	used := make([]bool, len(strs))
+	backtrack([]string{}, strs, &res, used)
+	return res
+}
+
+func backtrack(cur []string, strs []string, res *[][]string, used []bool) {
+	if len(cur) == len(strs) {
+		temp := make([]string, len(cur))
+		copy(temp, cur)
+		*res = append(*res, temp)
+		return
+	}
+	for i := 0; i < len(strs); i++ {
+		if used[i] {
+			continue
+		}
+		used[i] = true
+		cur = append(cur, strs[i])
+		backtrack(cur, strs, res, used)
+		used[i] = false
+		cur = cur[:len(cur)-1]
+	}
 }
---
+++ core/tablestore_query_optimizer_test.go
@@ -0,0 +1,30 @@
+package core
+
+import "testing"
+
+func TestSimpleEqQueryOptimizer_GeneratePlan(t *testing.T) {
+	_ = []string{"sender", "receiver", "create_time", "msg_type", "cost"}
+
+	tbInfo := Table{
+		TableName: "test-table",
+		TableId:   1,
+		Columns:   []ColumnInfo{},
+		Indexs: []Index{{
+			IndexId:     0,
+			ColumnNames: []string{"receiver", "create_time"},
+			Enable:      true,
+		}, {
+			IndexId:     0,
+			ColumnNames: []string{"sender", "create_time"},
+			Enable:      false,
+		}, {
+			IndexId:     0,
+			ColumnNames: []string{"sender", "receiver", "create_time"},
+			Enable:      false,
+		}},
+	}
+
+	qr := NewSimpleEqQueryOptimizer()
+	res := qr.GeneratePlan([]string{"sender", "receiver"}, tbInfo)
+	t.Log(res)
+}
